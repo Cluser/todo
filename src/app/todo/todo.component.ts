88701e2347da23db68122c201b38396c6b570888
Cluser <mrbfilipiak@gmail.com>
2020-07-15 01:35:32 +0200

    Add global style

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -18,24 +18,24 @@ export class TodoComponent implements OnInit {
         name: 'New',
         elements:
         [
+          // { id: 0, title: 'Title', description: 'description', date: new Date()},
           { id: 0, title: 'Title', description: 'description', date: new Date()},
-          { id: 0, title: 'Title', description: 'description', date: new Date()},
-          { id: 0, title: 'Title', description: 'description', date: new Date()}
+          // { id: 0, title: 'Title', description: 'description', date: new Date()}
         ]
       },
       inProgress: {
         name: 'In progress',
         elements:
         [
-          { id: 0, title: 'Title', description: 'description', date: new Date()},
-          { id: 0, title: 'Title', description: 'description', date: new Date()}
+          // { id: 0, title: 'Title', description: 'description', date: new Date()},
+          // { id: 0, title: 'Title', description: 'description', date: new Date()}
         ]
       },
       done: {
         name: 'Done',
         elements:
         [
-          { id: 0, title: 'Title', description: 'description', date: new Date()}
+          // { id: 0, title: 'Title', description: 'description', date: new Date()}
         ]
       }
     };
